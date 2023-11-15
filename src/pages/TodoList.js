d91a6e22db7de2ab63b8bc53de655d6c5f724207
hacker <profseur@.com>
2023-11-15 07:51:07 +0000

    mock-user

--- src/pages/TodoList.js
+++ src/pages/TodoList.js
@@ -1,42 +1,6 @@
 import { useState, useEffect } from "react";
+import TACHES from "../models/mock-user";
 
-const TACHES = [
-  {
-    id: 1,
-    tache: "Faire les courses",
-    enGras: true,
-    terminee: false,
-    date: "2023-11-06",
-  },
-  {
-    id: 2,
-    tache: "Répondre aux emails",
-    enGras: false,
-    terminee: true,
-    date: "2023-11-07",
-  },
-  {
-    id: 3,
-    tache: "Préparer la présentation",
-    enGras: true,
-    terminee: false,
-    date: "2023-11-08",
-  },
-  {
-    id: 4,
-    tache: "Réunion avec l'équipe",
-    enGras: false,
-    terminee: false,
-    date: "2023-11-09",
-  },
-  {
-    id: 5,
-    tache: "Exercices de yoga",
-    enGras: false,
-    terminee: true,
-    date: "2023-11-10",
-  },
-];
 
 function TodoList() {
   //state
